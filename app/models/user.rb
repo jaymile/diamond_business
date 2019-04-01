d50589eb462926049e660048e29af06f59c6a61f
jaymile <djemil.aminou@gmail.com>
2019-04-01 18:34:26 +0200

    relation user, event, attend ances, et subscription ainsi que generation de la view user par devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         after_create :welcome_send
+
+         has_many :attendances, foreign_key: 'guest_id'
+         has_many :events, foreign_key: 'admin_id'
+         has_many :subscription, foreign_key: 'admin_id'
+
+        #  validates :first_name,
+        #    presence: true
+        #  validates :last_name,
+        #    presence: true
+           
+     def welcome_send
+       UserMailer.welcome_email(self).deliver_now
+     end      
 end
