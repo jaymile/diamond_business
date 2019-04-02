f0d07d5102248354ba8985c3dc764c2df456df4c
jaymile <djemil.aminou@gmail.com>
2019-04-02 13:22:52 +0200

    une parti du controller de la participation est fait

--- app/controllers/participation_controller.rb
+++ app/controllers/participation_controller.rb
@@ -1,2 +1,15 @@
 class ParticipationController < ApplicationController
+    def index 
+        @participants = Attendance.where(event_id: params[:event_id])
+        puts @participants
+        @event = Event.find(params[:event_id])
+      end
+      
+      def new
+        @event = Event.new
+        
+        ##to display amount on page
+        @amount = Event.find(params[:event_id]).price
+      end
+
 end
